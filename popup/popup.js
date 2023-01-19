8508a1ec1fc445aa55b20c13cda26cc862210a29
Nicholas Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2023-01-19 10:56:26 -0500

    Format

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,11 @@
 // Get manifest data
-const manifestData = chrome.runtime.getManifest()
+const manifestData = chrome.runtime.getManifest();
 
 const setVersion = () => {
-    const version = manifestData.version
-    document.getElementById("version").innerHTML = "Version: " + version
-}
+  const version = manifestData.version;
+  document.getElementById("version").innerHTML = "Version: " + version;
+};
 
 window.onload = () => {
-    setVersion();
-}
+  setVersion();
+};
