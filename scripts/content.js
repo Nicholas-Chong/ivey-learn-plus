7d0bfe39684a13cd36d8b2399c6f3958fd24a856
Nicholas Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2023-01-25 13:39:21 -0500

    Format

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // Function to show hidden calendar element
 const showCalendar = () => {
   document.getElementById("dpBookARoom").style.display = "block";
@@ -28,26 +29,27 @@ const markWindowRooms = () => {
 };
 
 const getTokenAndSession = () => {
-  const token = document.getElementById('token').value;
-  const sessionValues = document.getElementById('hdnSessionValues').value;
+  const token = document.getElementById("token").value;
+  const sessionValues = document.getElementById("hdnSessionValues").value;
 
   return [token, sessionValues];
-}
+};
 
 // Function to fetch user bookings
-const showBookings = async() => {
+const showBookings = async () => {
   const parser = new DOMParser();
   const [token, sessionValues] = getTokenAndSession();
-  const userBookings = await fetch(`https://apps2.ivey.ca/lti/RoomBooking/MyBookings/MyBookings?token=${token}&sessionValues=${sessionValues}`)
-    .then(userBookings => userBookings.text())
-    .then( userBookings => {
-      const doc = parser.parseFromString(userBookings, "text/html")
-      return doc.getElementsByClassName('col-md-12')[0].innerHTML
-    }
-);
+  const userBookings = await fetch(
+    `https://apps2.ivey.ca/lti/RoomBooking/MyBookings/MyBookings?token=${token}&sessionValues=${sessionValues}`
+  )
+    .then((userBookings) => userBookings.text())
+    .then((userBookings) => {
+      const doc = parser.parseFromString(userBookings, "text/html");
+      return doc.getElementsByClassName("col-md-12")[0].innerHTML;
+    });
 
-  document.getElementById('dpBookARoom').innerHTML += userBookings;
-}
+  document.getElementById("dpBookARoom").innerHTML += userBookings;
+};
 
 // Function to delete user bookings
 const deleteBooking = (event) => {
@@ -55,51 +57,51 @@ const deleteBooking = (event) => {
   const bookingId = button.getAttribute("data-rb");
   const studentDetailId = document.getElementById("mlid").value;
   const [token, sessionValues] = getTokenAndSession();
-  const url = `https://apps2.ivey.ca/lti/RoomBooking/MyBookings/DeleteRoomBooking?resourceBookingId=${bookingId}&studentDetailId=${studentDetailId}`
-  
+  const url = `https://apps2.ivey.ca/lti/RoomBooking/MyBookings/DeleteRoomBooking?resourceBookingId=${bookingId}&studentDetailId=${studentDetailId}`;
+
   const headers = {
-    'accept': '*/*',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9',
-    'content-length': '0',
-    'origin': 'https://apps2.ivey.ca',
-    'sessionvalues': sessionValues,
-    'token': token,
-    'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-    'x-requested-with': 'XMLHttpRequest',
+    accept: "*/*",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "content-length": "0",
+    origin: "https://apps2.ivey.ca",
+    sessionvalues: sessionValues,
+    token,
+    "sec-ch-ua": '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+    "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+    "x-requested-with": "XMLHttpRequest",
   };
 
   const options = {
-    method: 'POST',
-    headers: headers,
+    method: "POST",
+    headers,
   };
 
   fetch(url, options)
-        .then(response => {
-          if (!response.ok) {
-            console.log(`Unable to delete booking, error status: ${response.status}`);
-          }
-          else {
-            console.log("Successfully deleted your booking!");
-          }
-          return response;
-        })
-        .catch(error => {
-          console.error(error);
-        });
-  
+    .then((response) => {
+      if (!response.ok) {
+        console.log(`Unable to delete booking, error status: ${response.status}`);
+      } else {
+        console.log("Successfully deleted your booking!");
+      }
+      return response;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   window.location.reload();
-}
+};
 
 // Call all enabled functions
 const callEnabledFunctions = () => {
   // Fetches settings from sync server and calls appropriate functions
   chrome.storage.sync.get(null, function (result) {
-    if(result["bookingsBox"]) showBookings();
-    if(result["calendarBox"]) showCalendar();
-    if(result["windowBox"]) markWindowRooms();
-  });  
+    if (result.bookingsBox) showBookings();
+    if (result.calendarBox) showCalendar();
+    if (result.windowBox) markWindowRooms();
+  });
 };
 
 callEnabledFunctions();
@@ -118,5 +120,5 @@ setTimeout(() => {
   const deleteContainers = document.getElementsByClassName("inline-block");
   for (let i = 0; i < deleteContainers.length; i++) {
     deleteContainers[i].addEventListener("click", deleteBooking, true);
-  };
+  }
 }, 1000); // delay to load DOM elements from showBookings function
